58f8b6a1f87839bdadb684e03d78bd23f11424b5
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-12-07 22:26:46 +0000

    Added an archive, created the dataset and config py files

--- discriminator_model.py
+++ discriminator_model.py
@@ -7,7 +7,7 @@ class Block(nn.Module):
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 4,stride, 1, bias=True, padding_mode = "reflect"),
-            nn.isinstanceNorm2d(out_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.LeakyReLU(0.2),
         )
         
@@ -38,14 +38,14 @@ class Discriminator(nn.Module):
         for feature in features[1:]:
             layers.append(Block(in_channels, feature, stride=1 if features==features[-1] else 2))
             in_channels = feature
-        layers.append(nn.Conv2d(in_channels, 1, kernal_size = 4, stride=1, padding=1, padding_mode = "reflect"))
+        layers.append(nn.Conv2d(in_channels, 1, kernel_size = 4, stride=1, padding=1, padding_mode = "reflect"))
         self.model = nn.Sequential(*layers)
         
         
         
-def forward(self, x):
-    x = self.inital(x)
-    return torch.sigmoid(self.model(x))
+    def forward(self, x):
+        x = self.inital(x)
+        return torch.sigmoid(self.model(x))
 
 
 def test():
